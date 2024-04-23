bae1518912d0e72f658fd123a3414b71a83c329b
Sveta <ghjk>
2024-04-23 08:19:39 +0500

    aa

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -6,13 +6,9 @@ import '../modules/home/views/home_view.dart';
 part 'app_routes.dart';
 
 class AppPages {
-  AppPages._();
-
-  static const INITIAL = Routes.HOME;
-
   static final routes = [
     GetPage(
-      name: _Paths.HOME,
+      name: Routes.HOME,
       page: () => const HomeView(),
       binding: HomeBinding(),
     ),
