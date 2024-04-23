634eb5bac7a372e13c377f876cf95ff9cd83d33c
Sveta <ghjk>
2024-04-23 09:28:55 +0500

    e

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,5 +1,7 @@
 import 'package:get/get.dart';
 
+import '../modules/chat/bindings/chat_binding.dart';
+import '../modules/chat/views/chat_view.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
 
@@ -11,6 +13,11 @@ class AppPages {
       name: Routes.HOME,
       page: () => const HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: Routes.CHAT,
+      page: () => const ChatView(),
+      binding: ChatBinding(),
     ),
   ];
 }
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -1,9 +1,7 @@
 part of 'app_pages.dart';
 
-
 abstract class Routes {
   static const HOME = '/';
   static const CHAT = '/chat';
+ 
 }
-
-
